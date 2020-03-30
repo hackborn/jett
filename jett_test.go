d3a750d0fdfb76ebe01e6981cd17178df07e9d1b
hackborn <hackborn@gmail.com>
2020-03-29 18:10:06 -0700

    cleanup

--- jett_test.go
+++ jett_test.go
@@ -2,7 +2,6 @@ package jett
 
 import (
 	"fmt"
-	"github.com/micro-go/lock"
 	"sync"
 	"sync/atomic"
 	"testing"
@@ -57,22 +56,18 @@ func runTestPool(t *testing.T, opts Opts, operations int, block *Block) {
 
 // Block blocks on messages.
 type Block struct {
-	runningWg        sync.WaitGroup
-	running          bool
-	mutex            sync.Mutex
-	want             int // How many items I want before I unblock.
-	have             int // How many items I have.
-	waiting          []chan BlockCmd
-	stage            BlockStage
-	accumulatingWg   sync.WaitGroup
-	accumulatingCond *mutexCond
-	runningCond      *mutexCond
+	runningWg      sync.WaitGroup
+	running        bool
+	mutex          sync.Mutex
+	want           int // How many items I want before I unblock.
+	have           int // How many items I have.
+	accumulatingWg sync.WaitGroup
+	runningCond    *mutexCond
 }
 
 func newBlock(want int) *Block {
-	accumulatingCond := newMutexCond()
 	runningCond := newMutexCond()
-	return &Block{want: want, stage: StageAccumulating, accumulatingCond: accumulatingCond, runningCond: runningCond}
+	return &Block{want: want, runningCond: runningCond}
 }
 
 func (b *Block) Handler(inner RunFunc) RunFunc {
@@ -90,8 +85,6 @@ func (b *Block) Handler(inner RunFunc) RunFunc {
 func (b *Block) Close() error {
 	fmt.Println("....block close??")
 	b.running = false
-	b.accumulatingCond.c.Broadcast()
-	//	b.wg.Wait()
 	return nil
 }
 
@@ -119,11 +112,6 @@ func (b *Block) handle(f RunFunc) error {
 		return nil
 	}
 
-	//	if b.handled(f) {
-	//		fmt.Println("handle() handled")
-	//		return nil
-	//	}
-
 	// Wait for everyone to accumulate
 	go func() {
 		fmt.Println("finish ACCUM WG 1")
@@ -136,36 +124,9 @@ func (b *Block) handle(f RunFunc) error {
 	return nil
 }
 
-func (b *Block) handled(f RunFunc) bool {
-	defer lock.Locker(&b.mutex).Unlock()
-	b.have += 1
-	fmt.Println("BLOCK have", b.have, "want", b.want)
-	if b.have >= b.want {
-		fmt.Println("block FLUSH")
-		// Flush
-		b.accumulatingCond.c.Broadcast()
-		/*
-			for _, c := range b.waiting {
-				c <- CmdRun
-			}
-		*/
-		f()
-		return true
-	}
-	return false
-}
-
 // init() performs necessary initialization dynamically.
 func (b *Block) init() {
 	b.running = true
-}
-
-func (b *Block) addWaitingChannel(c chan BlockCmd) {
-	defer lock.Locker(&b.mutex).Unlock()
-	b.waiting = append(b.waiting, c)
-}
-
-func (b *Block) waitingChannelFinished(c chan BlockCmd) {
 }
 
 // ------------------------------------------------------------
@@ -176,15 +137,6 @@ type BlockCmd uint32
 const (
 	CmdRun BlockCmd = 1 << iota
 	CacheGetter
-)
-
-type BlockStage uint32
-
-const (
-	StageAccumulating BlockStage = 1 << iota
-	StageReadyToRun
-	StageRunning
-	StageDone
 )
 
 // ------------------------------------------------------------
